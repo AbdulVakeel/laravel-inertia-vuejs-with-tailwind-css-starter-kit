43123dc8e3621265ccfc22e4ca44338f584a9dcd
Abdul Vakeel Software Engineer <163490858+AbdulVakeel@users.noreply.github.com>
2025-11-22 16:25:34 +0530

    Update app.php

--- config/app.php
+++ config/app.php
@@ -6,7 +6,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Name
+    | Application Name --
     |--------------------------------------------------------------------------
     |
     | This value is the name of your application. This value is used when the
