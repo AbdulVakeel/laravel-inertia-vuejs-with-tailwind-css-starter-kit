4179b9dda5c8edc9d85b23b64bd3e029bdc0f528
AbdulVakeel <abdulvakeel@skycodelab.in>
2026-05-06 11:31:30 +0530

    UserDashboardController  Update

--- app/Http/Controllers/UserDashboardController.php
+++ app/Http/Controllers/UserDashboardController.php
@@ -4,46 +4,43 @@ namespace App\Http\Controllers;
 
 use App\Models\Faq;
 use Inertia\Inertia;
+
 class UserDashboardController extends Controller
 {
-
     /***********************************
-     * Start THIS IS A User Dashboard  Controller *
+     * User Dashboard Controller
      ***********************************/
 
+    /**
+     * Dashboard
+     */
+    public function dashboardUser()
+    {
+        $user = auth()->user();
+
+        $data['pageTitle']      = "Dashboard";
+        $data['deposit_wallet'] = $user->deposit_wallet ?? 0;
+        $data['earning_wallet'] = $user->earning_wallet ?? 0;
+
+        return Inertia::render('User/Dashboard/UserDashboard', [
+            'data' => $data,
+            'user' => $user,
+        ]);
+    }
 
     /**
-     * dashboardUser
-     *
-     * @return void
+     * FAQ Page
      */
-
-
-    public function dashboardUser()
-{
-    $user = auth()->user();
-    
-    $data['pageTitle'] = "Dashboard";
-    $data['deposit_wallet'] = $user->deposit_wallet ?? 0;
-    $data['earning_wallet'] = $user->earning_wallet ?? 0;
-    
-    return inertia('User/Dashboard/UserDashboard', [
-        'data' => $data,
-        'user' => $user,
-    ]);
-}
-
-       public function faq()
+    public function faq()
     {
-        $data['faqs'] = Faq::orderBy('id', 'asc')->get(); 
+        $data['faqs'] = Faq::orderBy('id', 'asc')->get();
 
         return Inertia::render('User/Faq/Index', [
             'data' => $data,
         ]);
     }
 
-
     /***********************************
-     * End THIS IS A User Dashboard  Controller *
+     * End User Dashboard Controller
      ***********************************/
 }
